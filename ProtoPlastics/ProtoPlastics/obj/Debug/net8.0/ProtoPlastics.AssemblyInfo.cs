9fcd8d8a4b1d7a459927267f0f5693fe9d3cc8c3
bolst <bolton21@uwindsor.ca>
2024-06-20 11:43:43 -0400

    idek

--- ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
+++ ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Megabit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright 2018-2024 Megabit")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("\r\n\t\t\tThis is a Blazorise Template for a Blazor Server Application.\r\n\t\t\tBlazorise " +
-    "is a component library built on top of Blazor and CSS frameworks like Bootstrap," +
-    " FluentUI2, Tailwind, Bulma and Material.\r\n\t\t")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("\n\t\t\tThis is a Blazorise Template for a Blazor Server Application.\n\t\t\tBlazorise is" +
+    " a component library built on top of Blazor and CSS frameworks like Bootstrap, F" +
+    "luentUI2, Tailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+a13334d86ce16ecdb78aeab4bb52ca3981cecb84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+cbb0ed245bf68f20344a955cba38abe719a00412")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
