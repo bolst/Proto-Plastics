0516abde26f8622e304b7418438f388ea2b05f8d
nic <bolton21@uwindsor.ca>
2024-06-21 10:13:13 -0400

    reformat services into components

--- ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
+++ ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like Bootstrap, F" +
     "luentUI2, Tailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+9fcd8d8a4b1d7a459927267f0f5693fe9d3cc8c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+5fb78c285ae2b944dfdd87b239d5237e582543ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
