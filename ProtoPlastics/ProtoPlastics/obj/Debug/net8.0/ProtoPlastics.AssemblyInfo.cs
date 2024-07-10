239368ee4e955085c8dbe9c910aaa256af176b9f
bolst <bolton21@uwindsor.ca>
2024-07-10 11:24:17 -0400

    update 404 page

--- ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
+++ ProtoPlastics/ProtoPlastics/obj/Debug/net8.0/ProtoPlastics.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
     " a component library built on top of Blazor and CSS frameworks like Bootstrap, F" +
     "luentUI2, Tailwind, Bulma and Material.\n\t\t")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+fb4eeefcf23bb36673b68c43603123a51742db59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.5+86842914e7bf10bf5d3375a673d679785cbd9313")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtoPlastics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.5.0.0")]
